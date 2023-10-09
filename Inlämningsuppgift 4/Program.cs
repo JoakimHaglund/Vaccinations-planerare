de4f2c93a7c483c5ede79898e1c80106d63c62aa
JoakimHaglund <joakimhaglund@outlook.com>
2023-10-09 10:38:53 +0200

    Fix for MSB3021

--- Inlämningsuppgift 4/Program.cs
+++ Inlämningsuppgift 4/Program.cs
@@ -112,8 +112,8 @@ namespace Vaccination
                     Console.WriteLine("Error in file read");
                     break;
                 }
-            }*/
-        }
+            }
+        }*/
 
 
     }
