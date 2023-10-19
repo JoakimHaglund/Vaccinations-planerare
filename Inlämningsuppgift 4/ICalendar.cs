ec8d94619868f4dea6eda5c943b4ce9a818d7c33
JoakimHaglund <joakimhaglund@outlook.com>
2023-10-19 13:07:06 +0200

    Created ICalendar class

---
+++ Inlämningsuppgift 4/ICalendar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICalendar
+{
+    public class ICalendar
+    {
+        public string ProdId;
+        public DateTime Stamp;
+        public DateTime CurrentEvent;
+        private Random randomNum = new Random();
+    }  
+}
